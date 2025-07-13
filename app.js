cbcb509fd66042f688ef39156486934d4d8b7bfd
ck <choonkiat.tai@gail.com>
2025-07-13 19:08:14 +0800

    2nd  commit

--- app.js
+++ app.js
@@ -10,8 +10,4 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "UP" });
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
 module.exports = app;
